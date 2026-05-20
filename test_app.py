d62bcb8980d4d3c36d810e2bce4cc2391d9090b2
Лози <bagrov-danil@bk.ru>
2026-05-20 18:53:45 +0300

    Добавлены основный файлы

---
+++ test_app.py
@@ -0,0 +1,12 @@
+import pytest
+from app import divide
+
+
+def test_divide():
+    assert divide(10, 2) == 5.0
+    assert divide(7, 2) == 3.5
+
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        divide(10, 0)
